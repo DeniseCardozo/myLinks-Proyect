b5c367a7ca9ccffc17a8d37187f0b56cf908778a
DeniseCardozo <dennicardozo96@gmail.com>
2022-08-18 14:59:54 -0300

    feature(client/src/components/NavBar): add vertical NavBar and apply styles

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import style from "./NavBar.module.css";
 
 export default function NavBar() {
     return (
         <React.Fragment>
-            <h1>I'am NavBar</h1>
+            <div className={style.header}>
+                <div className={style.navMenu}>   
+                    <div className={style.links}>
+                        <h5>FAVOURITE</h5>
+                    </div>     
+                    <div className={style.links}>
+                        <h5>User</h5>
+                    </div>                    
+                </div>
+            </div> 
         </React.Fragment>
     )
 }
