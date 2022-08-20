1cf471f90e1e73f376c0b6150bc862ebdfe5ad33
DeniseCardozo <dennicardozo96@gmail.com>
2022-08-20 13:13:48 -0300

    feature(client/src/components/Box -/image): add new buttons - apply styles to buttons and input

--- client/src/components/Box/Box.jsx
+++ client/src/components/Box/Box.jsx
@@ -3,6 +3,10 @@ import Link from "../Link/Link";
 import styles from "./Box.module.css"
 import deleteimg from "../../image/delete.png";
 import edit from "../../image/edit.png";
+import add from "../../image/add.png";
+import ok from "../../image/ok.png";
+import cancel from "../../image/cancel.png";
+
 import { useDispatch } from "react-redux";
 import { putNameBox } from "../../redux/actions";
 
@@ -32,47 +36,53 @@ export default function Box({box, setUpdateList, updateList}) {
     return (
         <React.Fragment>
             <div className={styles.principalBox}>
-                <div className={styles.nameBox}>
-                    
                     {
                         showMiniForm ? 
-                        <div>
-                        <form onSubmit={(e) => handleSubmit(e)}>
-                            <input type="text" value={input.name} name="name" onChange={(e) => handleChange(e)} />
-                            <button type="submit">OK</button>
-                            <button onClick={(e) => handleClick(e)}>CANCEL</button>
-                        </form>
-                        </div> :
-                        <>
-                            <h1 className={styles.nameTitle}>{box.name}</h1>
-                            <div className={styles.buttonBox}>
-                                <div className={styles.subButtonBox}>
-                                    <img
-                                        src={edit}
-                                        alt="buttonedit"
-                                        className={styles.button} type="button" 
-                                        onClick={handleClick}
-                                    />
-                                    <img
-                                        src={deleteimg}
-                                        alt="buttondelete"
-                                        className={styles.button} type="button" 
-                                    />
-                                </div>  
+                        <form onSubmit={(e) => handleSubmit(e)} className={styles.formBox}>
+                            <input type="text" value={input.name} name="name" className={styles.inputName} onChange={(e) => handleChange(e)} />
+                            <div className={styles.buttonBox2}>
+                                <button className={styles.buttonDefault} type="submit">                                <img
+                                    src={ok}
+                                    alt="buttonOk"
+                                    className={styles.button} type="button" 
+                                />
+                                </button>
+
+                                <img
+                                    src={cancel}
+                                    alt="buttonCancel"
+                                    className={styles.button} type="button"  onClick={(e) => handleClick(e)}
+                                />
                             </div>
-                        </>
+                        </form> :
+                            <div className={styles.nameBox}>                           
+                                <h1 className={styles.nameTitle}>{box.name}</h1>
+                                <div className={styles.buttonBox}>
+                                    <div className={styles.subButtonBox}>
+                                        <img
+                                            src={add}
+                                            alt="buttonadd"
+                                            className={styles.button} type="button" 
+                                        />
+                                        <img
+                                            src={edit}
+                                            alt="buttonedit"
+                                            className={styles.button} type="button" 
+                                            onClick={handleClick}
+                                        />
+                                        <img
+                                            src={deleteimg}
+                                            alt="buttondelete"
+                                            className={styles.button} type="button" 
+                                        />
+                                    </div>  
+                                </div>
+                            </div>
                     }
-                  
-                </div>
-               
                 {
                     box.links && 
                     box.links.map((link) => 
                     <Link key={link.id_link} link={link} />)
-                    //<div key={link.id_link}>
-                      //  <h5>{link.name}</h5>
-                      //  <h5>{link.url_link}</h5>
-                    //</div>)
                 }
             </div>
         </React.Fragment>
