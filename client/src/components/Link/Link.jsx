0633f409ad728ee4d69c3adcb15c02bf9299ca28
DeniseCardozo <dennicardozo96@gmail.com>
2022-08-19 23:41:36 -0300

    refactor(client/src/components/Link -/Box): apply styles to buttons

--- client/src/components/Link/Link.jsx
+++ client/src/components/Link/Link.jsx
@@ -14,39 +14,50 @@ export default function Link({link}) {
     return (
         <React.Fragment>
             <div className={styles.principalBox}>
-                <div className={styles.secundaryBox}>
-                <h5 className={styles.nameTitle}>{link.name}</h5>
-                <div className={styles.buttonBox}>
-                    <img
-                        src={edit}
-                        alt="buttonedit"
-                        className={styles.button} type="button" 
-                        onClick={handleClick}
-                    />
-                    <img
-                        src={deleteimg}
-                        alt="buttondelete"
-                        className={styles.button} type="button" 
-                    />
-                </div>
-                </div>
-
-                {/* este div tiene que aparece luego de que le hagan clic al boton de edit */}
-                { showMiniForm &&
+                { showMiniForm ?
                     <div className={styles.editBox}>
-                    <input
-                        type="text"
-                        value={link.name}
-                        name="name"
-                    />
-                    <input
-                        type="text"
-                        value={link.url_link}
-                        name="url_link"
-                    />
-                    </div>
+                        <form>
+                            <input
+                                type="text"
+                                value={link.name}
+                                name="name"
+                            />
+                            <input
+                                type="text"
+                                value={link.url_link}
+                                name="url_link"
+                            />
+                            <button type="submit">OK</button>
+                            <button onClick={(e) => handleClick(e)}>CANCEL</button>
+
+                        </form>
+                    </div> :
+                    <div className={styles.secundaryBox}>
+                        <h5 className={styles.nameTitle}>{link.name}</h5>
+                        <div className={styles.buttonBox}>
+                            <div className={styles.subButtonBox}>
+
+                                <img
+                                    src={edit}
+                                    alt="buttonedit"
+                                    className={styles.button} type="button" 
+                                    onClick={handleClick}
+                                />
+                                <img
+                                    src={deleteimg}
+                                    alt="buttondelete"
+                                    className={styles.button} type="button" 
+                                />
+                            </div>
+                        </div>
+                        </div>
                 }
-             
+
+
+
+              
+
+    
                
 
             </div>
