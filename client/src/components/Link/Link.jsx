f6365f045b2dd75b30e855359fcec0293acb6c96
DeniseCardozo <dennicardozo96@gmail.com>
2022-08-20 13:41:42 -0300

    refactor(client/src/components/Link): add styles to buttons and input

--- client/src/components/Link/Link.jsx
+++ client/src/components/Link/Link.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import styles from "./Link.module.css"
 import deleteimg from "../../image/delete.png";
 import edit from "../../image/edit.png";
+import ok from "../../image/ok.png";
+import cancel from "../../image/cancel.png";
 
 
 export default function Link({link}) {
@@ -15,23 +17,38 @@ export default function Link({link}) {
         <React.Fragment>
             <div className={styles.principalBox}>
                 { showMiniForm ?
-                    <div className={styles.editBox}>
-                        <form>
-                            <input
-                                type="text"
-                                value={link.name}
-                                name="name"
-                            />
-                            <input
-                                type="text"
-                                value={link.url_link}
-                                name="url_link"
-                            />
-                            <button type="submit">OK</button>
-                            <button onClick={(e) => handleClick(e)}>CANCEL</button>
+                        <form className={styles.formBox}>
+                            <div className={styles.inputsBox}>
+                                <input
+                                    type="text"
+                                    value={link.name}
+                                    name="name"
+                                    className={styles.input} 
+                                />
+                                <input
+                                    type="text"
+                                    value={link.url_link}
+                                    name="url_link"
+                                    className={styles.input}
+                                />
+                            </div>
+                            <div className={styles.buttonBox2}>
 
-                        </form>
-                    </div> :
+                                <button className={styles.buttonDefault} type="submit">                                
+                                    <img
+                                        src={ok}
+                                        alt="buttonOk"
+                                        className={styles.button} type="button" 
+                                    />
+                                </button>
+
+                                <img
+                                    src={cancel}
+                                    alt="buttonCancel"
+                                    className={styles.button} type="button"  onClick={(e) => handleClick(e)}
+                                /> 
+                            </div>
+                        </form> :
                     <div className={styles.secundaryBox}>
                         <h5 className={styles.nameTitle}>{link.name}</h5>
                         <div className={styles.buttonBox}>
