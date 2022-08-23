ffadf2eb9195d2766ea74262c756e99ca700b04f
DeniseCardozo <dennicardozo96@gmail.com>
2022-08-22 22:05:38 -0300

    feature(client/src/components/NavBar - /image): add new logo to navBar

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import style from "./NavBar.module.css";
+import logo from "../../image/FavouriteLogo.png"
 
 export default function NavBar() {
     return (
         <React.Fragment>
             <div className={style.header}>
-                <div className={style.navMenu}>   
-                    <div className={style.links}>
-                        <h5>FAVOURITE</h5>
-                    </div>     
+                <div className={style.navMenu}>  
+                    <img
+                        src={logo}
+                        alt="logoFavoutrite"
+                        className={style.logo} type="button" 
+                    />
                     <div className={style.links}>
                         <h5>User</h5>
                     </div>                    
