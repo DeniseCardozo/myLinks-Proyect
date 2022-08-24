c3954dda5d1e461f9420e13db527de6af5d6ba9c
DeniseCardozo <dennicardozo96@gmail.com>
2022-08-24 15:03:09 -0300

    fix(api/src/routes/LinkRoutes.js): allow repeated post link

--- api/src/routes/LinkRouter.js
+++ api/src/routes/LinkRouter.js
@@ -6,21 +6,21 @@ router.post("/:id_box", async (req, res) => {
     try {
         const {id_box} = req.params;
         const {name, url_link} = req.body;
-        let findLink = await Link.findAll({
-            where:{
-                url_link: url_link
-            }
-        })
-        if(findLink.length > 0) {
-            res.status(403).send("Link could not be created, link already in use")
-        } else {
+        // let findLink = await Link.findAll({
+        //     where:{
+        //         url_link: url_link
+        //     }
+        // })
+        // if(findLink.length > 0) {
+        //     res.status(403).send("Link could not be created, link already in use")
+        // } else {
             let newLink = await Link.create({
                 name,
                 url_link,
                 id_box: id_box
         });
         newLink ? res.status(200).send("Created Link!") : res.status(400).send("Link not created");
-        }
+        // }
     } catch (error) {
         console.log(error)
     }
