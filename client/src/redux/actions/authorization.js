4d24b6b0ea8f50f4b61f1d09e23a42fc0763aa29
DeniseCardozo <dennicardozo96@gmail.com>
2022-08-24 15:20:09 -0300

    feature(client/src/redux): prepare redux for authorizations

--- client/src/redux/actions/authorization.js
+++ client/src/redux/actions/authorization.js
@@ -2,6 +2,24 @@ import axios from "axios";
 
 const urlAuth = "http://localhost:3001/auth";
 
+export const AUTHENTICATED = "AUTHENTICATED";
+export const NOT_AUTHENTICATED = "NOT_AUTHENTICATED";
+
+
+export const setToken = (token) => {
+    return localStorage.setItem("token", token);
+};
+  
+export const getToken = () => {
+    return localStorage.getItem("token");
+};
+  
+export const deleteToken = () => {
+    return localStorage.removeItem("token");
+};
+
+///////////////////
+
 export const postNewUser = (inputValue) => async () => {
     try {
         const input = {
@@ -16,3 +34,22 @@ export const postNewUser = (inputValue) => async () => {
         console.log(error)
     }
 };
+
+export const postLogin = (inputValue) => async (dispatch) => {
+    try {
+        const input = {
+            e_mail: inputValue.e_mail,
+            password: inputValue.password
+        };
+        const response = await axios
+        .post(`${urlAuth}/login`, input);
+        const token = response.data.token;
+        setToken(token);
+        dispatch({
+            type: AUTHENTICATED,
+            payload: response.data.user
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -1,10 +1,11 @@
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
 import {composeWithDevTools} from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "../reducer";
+import auth from "../reducer/authorization";
 
 const store = createStore(
-    rootReducer,
+    combineReducers({ rootReducer, auth }),
     composeWithDevTools(applyMiddleware(thunk))
 );
 
